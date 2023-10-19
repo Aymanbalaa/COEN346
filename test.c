d9de2ae0d85eaa146ea53c55d044f11754589652
Ayman El Balaa <84538300+Aymanbalaa@users.noreply.github.com>
2023-10-19 18:26:22 -0400

    Update test.c

--- test.c
+++ test.c
@@ -3,8 +3,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void printProcessInfo(int id) {
-    printf("[ID = %d] My parent is [%d]\n", getpid(), id);
+void printProcessInfo() {
+    printf("[ID = %d] My parent is [%d]\n", getpid(), getppid());
 }
 
 int main() {
@@ -15,35 +15,39 @@ int main() {
     pid_t pid_C1 = fork();
     if (pid_C1 == 0) {
         // C1 process
-        printProcessInfo(getppid());
+        printProcessInfo();
 
         // Create grandchild C2
         pid_t pid_C2 = fork();
         if (pid_C2 == 0) {
             // C2 process
-            printProcessInfo(getppid());
-        } else {
+            printProcessInfo();
+        }
+        else {
             // C1 process waits for C2 to finish
             wait(NULL);
         }
-    } else {
+    }
+    else {
         // Parent process
         // Create child C3
         pid_t pid_C3 = fork();
         if (pid_C3 == 0) {
             // C3 process
-            printProcessInfo(getppid());
+            printProcessInfo();
 
             // Create grandchild C4
             pid_t pid_C4 = fork();
             if (pid_C4 == 0) {
                 // C4 process
-                printProcessInfo(getppid());
-            } else {
+                printProcessInfo();
+            }
+            else {
                 // C3 process waits for C4 to finish
                 wait(NULL);
             }
-        } else {
+        }
+        else {
             // Parent process waits for all child processes to finish
             while(wait(NULL) > 0);
         }
