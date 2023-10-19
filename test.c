e0b388bd6ef5822058bb734e5e7af161591f8c5c
Ayman El Balaa <84538300+Aymanbalaa@users.noreply.github.com>
2023-10-19 18:58:42 -0400

    Update test.c

--- test.c
+++ test.c
@@ -1,57 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void printProcessInfo() {
-    printf("[ID = %d] My parent is [%d]\n", getpid(), getppid());
-}
-
-int main() {
-    printf("COEN 346 OPERATING SYSTEMS\n");
-    printf("[ID = %d] I am the root parent\n", getpid());
-
-    // Create child C1
-    pid_t pid_C1 = fork();
-    if (pid_C1 == 0) {
-        // C1 process
-        printProcessInfo();
-
-        // Create grandchild C2
-        pid_t pid_C2 = fork();
-        if (pid_C2 == 0) {
-            // C2 process
-            printProcessInfo();
-        }
-        else {
-            // C1 process waits for C2 to finish
-            wait(NULL);
-        }
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <integer>\n", argv[0]);
+        return 1;
     }
-    else {
-        // Parent process
-        // Create child C3
-        pid_t pid_C3 = fork();
-        if (pid_C3 == 0) {
-            // C3 process
-            printProcessInfo();
 
-            // Create grandchild C4
-            pid_t pid_C4 = fork();
-            if (pid_C4 == 0) {
-                // C4 process
-                printProcessInfo();
-            }
-            else {
-                // C3 process waits for C4 to finish
-                wait(NULL);
-            }
+    int N1 = atoi(argv[1]);
+
+    // Fork the first child process to compute the sum of positive integers
+    pid_t pid_sum = fork();
+    if (pid_sum == 0) {
+        int sum = 0;
+        for (int i = 1; i <= N1; ++i) {
+            sum += i;
         }
-        else {
-            // Parent process waits for all child processes to finish
-            while(wait(NULL) > 0);
-        }
+        printf("[ID = %d] Sum of positive integers up to %d is %d\n", getpid(), N1, sum);
+        exit(0);
     }
+
+    // Fork the second child process to compute the factorial
+    pid_t pid_fact = fork();
+    if (pid_fact == 0) {
+        int factorial = 1;
+        for (int i = 1; i <= N1; ++i) {
+            factorial *= i;
+        }
+        printf("[ID = %d] Factorial of %d is %d\n", getpid(), N1, factorial);
+        exit(0);
+    }
+
+    // Parent process waits for both children to finish
+    waitpid(pid_sum, NULL, 0);
+    waitpid(pid_fact, NULL, 0);
+
+    // Parent process prints the completion message
+    printf("[ID = %d] Done\n", getpid());
 
     return 0;
 }
