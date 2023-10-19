51f384589988b30dae1e7a8fcc2f6e1ece0810e5
Ayman El Balaa <84538300+Aymanbalaa@users.noreply.github.com>
2023-10-19 15:51:27 -0400

    Update Activity2.c

--- LABS/LAB2/Activity2.c
+++ LABS/LAB2/Activity2.c
@@ -1,49 +1,45 @@
 #include<sys/wait.h>
 #include<unistd.h>
 #include <stdio.h>
-
 int main()
 {
-// num is input
-// n is process 1
-// sum is where we will store the calc
-  
-int num;
+
+int input;
 int n;
 int sum=0;
 printf("Enter a number:");
-scanf("%d",&num);
+scanf("%d",&input);
 n = fork();
 if(n==0)
 {
-for(int i=1;i<=num;i++)
+for(int i=1;i<=input;i++)
 {
 sum=sum+i;
 }
-printf("The sum of numbers is %d\n", sum);
-printf("The identifier of first child process is %d\n",getpid());
+printf("[ID = " getpid() +"]" + "Sum of positive integers up to " + input +" is %d\n", sum);
+
 }
-// m for process 2
-// fact for calc
-int m =0;
-int fact =1;
+
+
+int m;
+int factorial=1;
 else
 {
 m = fork();
 if(m==0)
 {
-for(int i=1;i<=num;i++)
+for(int i=1;i<=input;i++)
 {
-fact=fact*i;
+factorial=factorial*i;
 }
-printf("The factorial of numbers is %d\n", fact);
-printf("The identifier of second child process is %d\n",getpid());
+
+printf("[ID = " getpid() +"]"+ "factorialorial of " + input + " is %d\n", factorial);
 }
 else
 {
 wait(NULL);
 printf("Done\n");
-//printf("The identifier of parent process is %d\n",getppid());
+printf("[ID = " getppid() +"]"+"Done\n");
 }
 }
 return 0;
