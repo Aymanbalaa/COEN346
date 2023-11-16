177087d26a255aef32d2c4d4fef8801dfa64cb50
Ayman El Balaa <84538300+Aymanbalaa@users.noreply.github.com>
2023-11-16 18:17:27 -0500

    Update Forkjoin.c

--- Forkjoin.c
+++ Forkjoin.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "common_threads.h"
-
+#include <pthread.h>
+#include <semaphore.h>
 
 sem_t s;
 
 void *child(void *arg) {
     printf("child\n");
-    // use semaphore here
-    sem_wait(&s);
-    sleep(1);
+    // Signal parent that child has completed
+    sem_post(&s);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
-    // init semaphore here
-    sem_init(&s, 1, 1);
     printf("parent: begin\n");
-    Pthread_create(&p, NULL, child, NULL);
-    // use semaphore here
+    
+    // Initialize semaphore with initial value 0
+    sem_init(&s, 0, 0);
+
+    // Create child thread
+    pthread_create(&p, NULL, child, NULL);
+
+    // Wait for the child to complete using semaphore
     sem_wait(&s);
+
+    // Destroy the semaphore
+    sem_destroy(&s);
+
     printf("parent: end\n");
     return 0;
 }
+
